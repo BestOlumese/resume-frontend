a69b593e1ee6cfe93d1a25afc296e71b8f6dbde4
Amiolemen Best <bestolumese@gmail.com>
2025-05-04 16:34:23 +0100

    updated patch

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import LandingPage from "./pages/LandingPage";
 import Login from "./pages/Auth/Login";
-// import Signup from "./pages/Auth/Signup";
+import Signup from "./pages/Auth/Signup";
 import Dashboard from "./pages/Home/Dashboard";
-import EditResume from "./pages/ResumeUpdate/editResume";
+import EditResume from "./pages/ResumeUpdate/EditResume";
 import UserProvider from "./context/userContext";
 
 const App = () => {
@@ -18,7 +18,7 @@ const App = () => {
             <Route path="/" element={<LandingPage />} />
 
             <Route path="/login" element={<Login />} />
-            {/* <Route path="/signup" element={<Signup />} /> */}
+            <Route path="/signup" element={<Signup />} />
 
             <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/resume/:resumeId" element={<EditResume />} />

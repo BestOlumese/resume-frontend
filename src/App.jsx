c48aec238217fb0859003c1d522797732ce4a89d
Amiolemen Best <bestolumese@gmail.com>
2025-05-04 16:31:19 +0100

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import LandingPage from "./pages/LandingPage";
 import Login from "./pages/Auth/Login";
-import Signup from "./pages/Auth/Signup";
+// import Signup from "./pages/Auth/Signup";
 import Dashboard from "./pages/Home/Dashboard";
 import EditResume from "./pages/ResumeUpdate/editResume";
 import UserProvider from "./context/userContext";
@@ -18,8 +18,8 @@ const App = () => {
             <Route path="/" element={<LandingPage />} />
 
             <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-            
+            {/* <Route path="/signup" element={<Signup />} /> */}
+
             <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/resume/:resumeId" element={<EditResume />} />
           </Routes>

a3b6e4b482bc279aa4b70afd1a0b12176101ba08
Amiolemen Best <bestolumese@gmail.com>
2025-05-04 16:28:12 +0100

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,7 @@ const App = () => {
 
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
+            
             <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/resume/:resumeId" element={<EditResume />} />
           </Routes>
